d229c479ed580af855688f3ba77e23198489a276
sakibtalukqder <sakibtalukqder07@gmail.com>
2023-11-19 04:28:07 +0600

    Icon 1.0.0

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,6 @@ import './style.css'
 import Navbar from '@/src/Shared/Navbar'
 import Footer from '@/src/Shared/Footer'
 
-import ICON from './favicon.ico'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -17,8 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href={`${ICON}?v=4`} type="image/v=4" sizes="32x32" />
-        <link rel="apple-touch-icon" href={"/apple-touch-icon.png?v=4"} type="image/v=4" sizes="32x32" />
+        <link rel="icon" href={'/favicon.ico?v=4'} type="image/v=4" sizes="32x32" />
       </head>
       <body className={inter.className}>
         <div className='mx-0 md:mx-12 lg:mx-16 xl:mx-20'>
--- app/route/contract/page.jsx
+++ app/route/contract/page.jsx
@@ -5,28 +5,28 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <div class="mx-16 my-8">
-                <section class="">
-                    <div class="flex flex-col md:flex-row justify-center">
-                        <div class="md:w-3/4 w-full mt-5">
-                            <h2 class="mb-6 text-3xl font-bold">Contact Me</h2>
-                            <p class="mb-6 md:max-w-xl">
+            <div className="mx-16 my-8">
+                <section className="">
+                    <div className="flex flex-col md:flex-row justify-center">
+                        <div className="md:w-3/4 w-full mt-5">
+                            <h2 className="mb-6 text-3xl font-bold">Contact Me</h2>
+                            <p className="mb-6 md:max-w-xl">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                 Laudantium, modi accusantium ipsum corporis quia asperiores
                                 dolorem nisi corrupti eveniet dolores ad maiores repellendus enim
                                 autem omnis fugiat perspiciatis? Ad, veritatis.
                             </p>
-                            <p class="mb-2 flex mt-1">
+                            <p className="mb-2 flex mt-1">
                                 <FontAwesomeIcon icon={faHome} width={20} className='me-3' />   Mujib Sarak, Sirajganj Sadre, Sirajganj
                             </p>
-                            <p class="mb-2 flex mt-1">
+                            <p className="mb-2 flex mt-1">
                                 <FontAwesomeIcon icon={faPhone} width={20} className='me-3' />  + 880 1799 232 910
                             </p>
-                            <p class="mb-2 flex mt-1">
+                            <p className="mb-2 flex mt-1">
                                 <FontAwesomeIcon icon={faAt} width={20} className='me-3' />     sakibtalukqder07@gmail.com
                             </p>
                         </div>
-                        <div class="mt-4 px-0 md:mb-0 w-full md:w-6/12 md:px-3 lg:px-6">
+                        <div className="mt-4 px-0 md:mb-0 w-full md:w-6/12 md:px-3 lg:px-6">
                             <form className="max-w-md mx-auto">
                                 <div className="mb-4">
                                     <label htmlFor="email" className="block text-sm font-medium text-gray-600">
