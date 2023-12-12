673ec38bfd747d86083ce161e4b4dd04dd4fee36
sakibtalukqder <sakibtalukqder07@gmail.com>
2023-12-13 01:16:35 +0600

    Google Search Console

--- app/layout.js
+++ app/layout.js
@@ -9,12 +9,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Sakib Talukqder',
   description: 'Personal portfolio build with next Js',
+  name: "google-site-verification",
+  content: "N2LZQg3R-UVVEO1wOzqTuGcyuRCECXDXglZNsXTDXek"
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="N2LZQg3R-UVVEO1wOzqTuGcyuRCECXDXglZNsXTDXek" />
         <link rel="icon" href={'/favicon.ico?v=4'} type="image/v=4" sizes="32x32" />
       </head>
       <body className={inter.className}>
